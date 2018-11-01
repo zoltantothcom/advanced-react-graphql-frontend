1256e11957c1d3e4f64d754be5e8b13fd3daf3d9
Zoltan Toth <contact@zoltantoth.com>
2018-11-01 14:56:40 -0400

    Removing cart items

---
+++ components/CartItem.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import RemoveFromCart from './RemoveFromCart';
+import formatMoney from '../lib/formatMoney';
+
+const CartItemStyles = styled.li`
+  padding: 1rem 0;
+  border-bottom: 1px solid ${props => props.theme.lightgrey};
+  display: grid;
+  align-items: center;
+  grid-template-columns: auto 1fr auto;
+
+  img {
+    margin-right: 10px;
+    max-width: 100px;
+  }
+
+  h3,
+  p {
+    margin: 0;
+  }
+`;
+
+const CartItem = ({ cartItem }) => (
+  <CartItemStyles>
+    <img src={cartItem.item.image} alt={cartItem.item.title} />
+    <div className="cart-item-details">
+      <h3>{cartItem.item.title}</h3>
+      <p>
+        {formatMoney(cartItem.item.price * cartItem.quantity)}
+        {' - '}
+        <em>
+          {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} Each
+        </em>
+      </p>
+    </div>
+    <RemoveFromCart id={cartItem.id} />
+  </CartItemStyles>
+);
+
+CartItem.propTypes = {
+  cartItem: PropTypes.object.isRequired,
+};
+
+export default CartItem;
